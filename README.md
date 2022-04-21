0a9253da30c5fa268c895fabbe622edeae5b24ce
Dan Webb <dan.webb@damacus.io>
2022-04-21 11:48:57 +0100

    Initial cut of Docker Action (#1)
    - Uses a built container
    - Runs on Ruby 3.1
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # publish-gem-to-github
-Release Ruby Gem to GitHub Packages
+
+Release your Ruby Gem to GitHub Packages
+
+This Action has been heavily influenced by [Jstastny's Publish-Gem-to-Github Action](https://github.com/jstastny/publish-gem-to-github)
+
+## Usage
+
+```yaml
+jobs:
+  release:
+    runs-on: ubuntu-latest
+      - name: Checkout
+        uses: actions/checkout@v2
+
+      - name: Build and publish to GitHub Package
+        uses: actionshub/publish-gem-to-github@master
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+          owner: ${{ secrets.OWNER }}
+```
---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+
+cd "${GITHUB_WORKSPACE}" || exit 1
+
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
+GITHUB_TOKEN="${1}"
+OWNER="${2}"
+WORKING_DIRECTORY="${3:-.}"
+
+[ -z "${GITHUB_TOKEN}" ] && { echo "Missing input.token!"; exit 2; }
+[ -z "${OWNER}" ] && { echo "Missing input.owner!"; exit 2; }
+
+echo "Setting up access to GitHub Package Registry"
+mkdir -p ~/.gem
+touch ~/.gem/credentials
+chmod 600 ~/.gem/credentials
+echo ":github: Bearer ${GITHUB_TOKEN}" >> ~/.gem/credentials
+
+cd "${WORKING_DIRECTORY}" || exit 1
+
+echo "Building gem"
+find . -name '*.gemspec' -maxdepth 1 -exec gem build {} \;
+echo "Pushing gem to GitHub Package Registry"
+find . -name '*.gem' -maxdepth 1 -exec gem push --key github --host "https://rubygems.pkg.github.com/${OWNER}" {} \;
